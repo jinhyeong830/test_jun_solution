9e8196f27489f3ef60f907508b4432c237ad6d42
jinhyeong830 <shg02387@naver.com>
2022-07-02 23:40:45 +0900

    miwansung

--- sol_11720/Main.java
+++ sol_11720/Main.java
@@ -1,9 +1,25 @@
 package sol_11720;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();// 숫자의 개수,,배열의 length,
+
+		char cArr[] = new char[n];
+
+		//char 를 입력받거나... string 을 제한하거나!?
+		
+		int sum = 0;
+		for (int j = 0; j < cArr.length; j++) {
+			
+			sum += cArr[j] - '0';// need char to integer
+		}
+		System.out.println(sum);
+
+		sc.close();
 
 	}
 
