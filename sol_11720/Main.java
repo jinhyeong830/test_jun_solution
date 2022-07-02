1c5c0f7490b6d38643bf162e5e70678132a0ba52
jinhyeong830 <shg02387@naver.com>
2022-07-02 23:58:24 +0900

    sol_11720 about String& charAt(n)

--- sol_11720/Main.java
+++ sol_11720/Main.java
@@ -7,15 +7,13 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();// 숫자의 개수,,배열의 length,
-
-		char cArr[] = new char[n];
-
+		String s=sc.next();
+		
 		//char 를 입력받거나... string 을 제한하거나!?
 		
 		int sum = 0;
-		for (int j = 0; j < cArr.length; j++) {
-			
-			sum += cArr[j] - '0';// need char to integer
+		for (int j = 0; j <n; j++) {
+			sum += s.charAt(j) - '0';// need char to integer
 		}
 		System.out.println(sum);
 
