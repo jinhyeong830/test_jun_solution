b40fa9c037805ac28ed937ae1e8de6cb2619e530
jinhyeong830 <shg02387@naver.com>
2022-07-12 18:17:51 +0900

    about String method, indexOf()

--- sol_10809/Main.java
+++ sol_10809/Main.java
@@ -11,6 +11,9 @@ public class Main {
 		for(char c='a';c<='z';c++) {	
 			System.out.print(s.indexOf(c)+" ");
 		}
+		
+		//char 도 for문에서 돌아갑니다
+		//indexOf() 는 문자열중 문자가 처음으로 나오는 위치를 반환하는 메소드입니다.
 	}
 
 }
