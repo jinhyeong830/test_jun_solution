9cd643e0f783d9395c28c767bc8b2a3e7ee682b8
jinhyeong830 <shg02387@naver.com>
2022-07-14 00:56:34 +0900

    find sosu count!

---
+++ silver_sol_1978/Main.java
@@ -0,0 +1,37 @@
+package silver_sol_1978;
+
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int tc = sc.nextInt();
+		int[] num = new int[tc];
+		for (int i = 0; i < tc; i++) {
+			num[i] = sc.nextInt();
+		}
+		sc.close();
+
+		int count=0;
+		for(int i=0;i<tc;i++) {
+			if(sosu(num[i])==true)
+				count++;
+		}
+		System.out.println(count);
+	}
+
+	public static boolean sosu(int n) {
+
+		int cnt = 0;
+		for (int i = 1; i < n + 1; i++) {
+			if (n % i == 0)
+				cnt++;
+		}
+		if (cnt == 2)
+			
+			return true;
+		else
+			return false;
+	}
+
+}
